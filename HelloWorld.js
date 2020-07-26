6086c9fdad4693bbcfb541829cb41499c4d3401d
Sokhibjon Orzikulov <genemators@gmail.com>
2020-07-26 18:26:01 +0500

    Let's do something unforgettable!
    There is your updated & enhanced hello world program!

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,8 @@
-const myFirstVariable = "Hello World";
+const startYear = 2020;
+const currentYear = new Date().getFullYear();
+const devTime = currentYear - startYear;
+
+console.log(
+  `Hello World after ${result} years!` +
+  `Let's create an unforgettable hello world!`
+)
